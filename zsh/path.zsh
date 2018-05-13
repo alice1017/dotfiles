0397eac693fd0eef282490b3d245cf2140008ea2
Alice1017 <genocidedragon@gmail.com>
2018-05-13 15:31:56 +0900

    zsh: setting of export path

---
+++ zsh/path.zsh
@@ -0,0 +1,11 @@
+#
+# PATH SECTION
+# ==================================================
+
+export PATH=$HOME/local/bin:$PATH
+export PATH=$HOME/local/go/bin:$PATH
+export PATH=$HOME/.anyenv/bin:$PATH
+export PATH=$HOME/.anyenv/envs/pyenv/versions/2.7.5/bin:$PATH
+
+export GOPATH=$HOME/local/go
+export FPATH=$HOME/.zsh/completions:$FPATH
